612edd46e3aaa8e29db03266887f1b5114579324
caglarkabaca <cacarekt@yandex.com>
2023-10-30 23:03:50 +0100

    merge sort ilk interface e

--- cpp/ASort.h
+++ cpp/ASort.h
@@ -1,21 +1,45 @@
 #ifndef ASort_h
 #define ASort_h
 #include <iostream>
+#include <vector>
 
 template<class RandomAccessIterator, class Less>
 void xyz_sort(RandomAccessIterator first, RandomAccessIterator beyond, Less less) {
-    RandomAccessIterator mut_beyond = beyond;
-    bool swapped;
-    do {
-        swapped = false;
-        for (RandomAccessIterator i = first; i != mut_beyond - 1; ++i) {
-            if(!less(*i, *(i + 1))) {
-                std::iter_swap(i, i + 1);
-                swapped = true;
-            }
+    auto size = std::distance(first, beyond);
+    if (size < 2)
+        return;
+    
+    RandomAccessIterator middle = std::next(first, size / 2);
+    
+    xyz_sort(first, middle, less);
+    xyz_sort(middle, beyond, less);
+    
+    // merge()
+    std::vector<typename RandomAccessIterator::value_type> arr;
+    
+    RandomAccessIterator l { first }, r { middle };
+    const RandomAccessIterator im_middle { middle }, im_beyond { beyond };
+    
+    while (l != im_middle && r != im_beyond) {
+        if (!less(*r, *l)) {
+            arr.push_back(*(l++));
+        } else {
+            arr.push_back(*(r++));
         }
-        mut_beyond--;
-    }while ( swapped );
+    }
+    
+    while (l != im_middle) {
+        arr.push_back(*(l++));
+    }
+    
+    while (r != im_beyond) {
+        arr.push_back(*(r++));
+    }
+    
+    RandomAccessIterator i_arr = arr.begin();
+    for (RandomAccessIterator i = first; i != beyond; ++i) {
+        std::iter_swap(i, i_arr++);
+    }
  }
 
 #endif
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,34 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <cassert>
 
 #include "ASort.h"
 
 int main() {
+    // create random vector
     std::vector<int> arr;
     std::random_device rd;
     std::mt19937 generator(rd());
     std::uniform_int_distribution<int> distribution(1, 1000);
+    
     for (int i = 0; i < 1000; ++i) {
-       int randomValue = distribution(generator);
-       arr.push_back(randomValue);
+        int randomValue = distribution(generator);
+        arr.push_back(randomValue);
     }
     
     xyz_sort(arr.begin(), arr.end(), std::less<int>());
-    
-    // print
-    bool is_sorted = true;
-    for (auto i = arr.begin(); i != arr.end() - 1; ++i) {
-        if (*i > *(i + 1)) {
-            is_sorted = false;
-            break;
-        }
-    }
-    if (is_sorted) {
-        std::cout << "sorted";
-    } else {
-        std::cout << "not sorted";
-    }
-    
-    std::cout << std::endl;
+
+    assert(not std::is_sorted(std::begin(arr), std::end(arr), std::greater_equal<>{}));
 }
