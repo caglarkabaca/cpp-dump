40278caf8b534c1a80b6d0dfabc4d7c0ccf801c2
caglarkabaca <cacarekt@yandex.com>
2023-10-31 13:40:17 +0100

    *cpp - diğer sort

--- cpp/include/ASort.h
+++ cpp/include/ASort.h
@@ -3,49 +3,65 @@
 
 #include <vector>
 
-template<class RandomAccessIterator, class Less>
-void xyz_sort(RandomAccessIterator first, RandomAccessIterator beyond, Less less) {
+template <class RandomAccessIterator, class Less>
+void xyz_sort(RandomAccessIterator first, RandomAccessIterator beyond, Less less)
+{
     int size = std::distance(first, beyond);
     if (size < 2)
         return;
-    
+
     RandomAccessIterator middle = std::next(first, size / 2);
-    
+
     xyz_sort(first, middle, less);
     xyz_sort(middle, beyond, less);
-    
+
     // merge()
     std::vector<typename RandomAccessIterator::value_type> arr;
-    RandomAccessIterator l { first }, r { middle };
-    const RandomAccessIterator im_middle { middle }, im_beyond { beyond };
-    
-    while (l != im_middle && r != im_beyond) {
-        if (!less(*r, *l)) {
+    RandomAccessIterator l{first}, r{middle};
+    const RandomAccessIterator im_middle{middle}, im_beyond{beyond};
+
+    while (l != im_middle && r != im_beyond)
+    {
+        if (!less(*r, *l))
+        {
             arr.push_back(*(l++));
-        } else {
+        }
+        else
+        {
             arr.push_back(*(r++));
         }
     }
-    
-    while (l != im_middle) {
+
+    while (l != im_middle)
+    {
         arr.push_back(*(l++));
     }
-    
-    while (r != im_beyond) {
+
+    while (r != im_beyond)
+    {
         arr.push_back(*(r++));
     }
-    
+
     RandomAccessIterator i_arr = arr.begin();
-    for (RandomAccessIterator i = first; i != beyond; ++i) {
+    for (RandomAccessIterator i = first; i != beyond; ++i)
+    {
         std::iter_swap(i, i_arr++);
     }
- }
-
+}
 
 template <class InputIterator, class OutputIterator, class Less>
-OutputIterator xyz_sort (InputIterator first,
-          InputIterator beyond,
-          OutputIterator result,
-          Less less );
+OutputIterator xyz_sort(InputIterator first, InputIterator beyond, OutputIterator result, Less less)
+{
+    std::vector<typename InputIterator::value_type> random_arr;
+    std::copy(first, beyond, std::back_inserter(random_arr));
+    xyz_sort(random_arr.begin(), random_arr.end(), less);
+
+    for (auto i = random_arr.rbegin(); i != random_arr.rend(); ++i)
+    {
+        *(--result) = *i;
+    }
+
+    return result;
+}
 
 #endif
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -5,6 +5,10 @@
 
 #include "ASort.h"
 
+#define SIZE 15
+
+#define _DEBUG
+
 int main()
 {
     // create random vector
@@ -13,14 +17,32 @@ int main()
     std::mt19937 generator(rd());
     std::uniform_int_distribution<int> distribution(1, 1000);
 
-    for (int i = 0; i < 1000; ++i)
+    for (int i = 0; i < SIZE; ++i)
     {
         int randomValue = distribution(generator);
         arr.push_back(randomValue);
     }
 
+    std::vector<int> arr_out(SIZE);
+
+    xyz_sort(arr.begin(), arr.end(), arr_out.end(), std::less<int>());
     xyz_sort(arr.begin(), arr.end(), std::less<int>());
 
+    assert(not std::is_sorted(std::begin(arr_out), std::end(arr_out), std::greater_equal<>{}));
     assert(not std::is_sorted(std::begin(arr), std::end(arr), std::greater_equal<>{}));
     std::cout << "Sorted" << std::endl;
+
+#ifdef _DEBUG
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+    {
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+
+    for (auto i = arr_out.begin(); i != arr_out.end(); ++i)
+    {
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+#endif
 }
